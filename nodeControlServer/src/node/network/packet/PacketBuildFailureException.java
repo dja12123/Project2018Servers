f470486bf2bb71579236f853e5d3648d6fab59b5
dja12123 <dja12123@dja-univ-main>
2018-09-25 18:27:59 +0900

    패킷 처리기 제작중

---
+++ nodeControlServer/src/node/network/packet/PacketBuildFailureException.java
@@ -0,0 +1,17 @@
+package node.network.packet;
+
+/**
+  * @FileName : PacketBuildFailureException.java
+  * @Project : Project2018Servers
+  * @Date : 2018. 9. 25. 
+  * @작성자 : dja12123
+  * @변경이력 :
+  * @프로그램 설명 : 패킷 빌드 실패 예외
+  */
+class PacketBuildFailureException extends Exception
+{
+	public PacketBuildFailureException(String msg)
+	{
+		super(msg);
+	}
+}
