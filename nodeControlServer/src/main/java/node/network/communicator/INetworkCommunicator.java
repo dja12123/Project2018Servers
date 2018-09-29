9f73a3e072bc7610add38fbeae8e67efd2fd9386
dja12123 <dja12123@dja-univ-main>
2018-09-29 16:23:51 +0900

    test

--- nodeControlServer/src/main/java/node/network/communicator/INetworkCommunicator.java
+++ nodeControlServer/src/main/java/node/network/communicator/INetworkCommunicator.java
@@ -1,5 +1,13 @@
 package node.network.communicator;
 
+/**
+  * @FileName : INetworkCommunicator.java
+  * @Project : nodeControlServer
+  * @Date : 2018. 9. 29. 
+  * @작성자 : dja12123
+  * @변경이력 :
+  * @프로그램 설명 :
+  */
 public interface INetworkCommunicator
 {
 	
