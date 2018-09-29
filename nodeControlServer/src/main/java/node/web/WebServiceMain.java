60710b1605d48fd80d828404c4937b5b10170266
Marlgolro <Marlgolro@Marlgolro>
2018-09-29 18:58:30 +0900

    웹서버 테스트 작성중

--- nodeControlServer/src/main/java/node/web/WebServiceMain.java
+++ nodeControlServer/src/main/java/node/web/WebServiceMain.java
@@ -2,20 +2,73 @@ package node.web;
 
 import node.IServiceModule;
 
-public class WebServiceMain implements IServiceModule
-{//test안녕태준아
-//test
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.util.ServerRunner;
+
+public class WebServiceMain extends NanoHTTPD implements IServiceModule 
+{
+	private static final Logger LOG = Logger.getLogger(WebServiceMain.class.getName());
+	
+	public WebServiceMain() {
+		super(80);
+	}
+	
+	@Override
+	public Response serve(IHTTPSession session) {
+		Method method = session.getMethod();
+		String uri = session.getUri();
+		WebServiceMain.LOG.info(method + " '" + uri + "' ");
+		
+		File readFile = new File("~/nanohttpd/www/index.html");
+		BufferedReader readBuffer = null;
+		try 
+		{
+			readBuffer = new BufferedReader(new FileReader(readFile));
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		String msg = null;
+		
+        try 
+        {
+			while ((msg = readBuffer.readLine()) != null) {}
+			readBuffer.close();
+        } 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+        catch (NullPointerException e1)
+        {
+        	e1.printStackTrace();
+        }
+
+
+		return newFixedLengthResponse(msg);
+	}
+	
 	@Override
 	public boolean start()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		ServerRunner.run(WebServiceMain.class);
+		return true;
 	}
 
 	@Override
 	public void stop()
 	{
-		// TODO Auto-generated method stub
+		
 		
 	}
 
