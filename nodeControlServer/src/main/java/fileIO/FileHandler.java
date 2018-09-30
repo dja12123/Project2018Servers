721d14e8296c8db162c21fdcc75355ffabf6cb63
mrtj0329 <mrtj0329@gmail.com>
2018-09-30 19:36:03 +0900

    파일리더 디버깅

--- nodeControlServer/src/main/java/fileIO/FileHandler.java
+++ nodeControlServer/src/main/java/fileIO/FileHandler.java
@@ -25,6 +25,7 @@ public class FileHandler {
         {
 			while ((tempReadString = bufRead.readLine()) != null) 
 			{
+				System.out.println(tempReadString);
 				fileReadString.append(tempReadString + "\n");
 			}
 			
