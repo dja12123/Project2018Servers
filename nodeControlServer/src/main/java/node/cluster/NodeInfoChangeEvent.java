1c07ab460a58d74b1ede07c4411de055f80444e3
dja12123 <dja12123@dja-univ-main>
2018-10-04 21:46:26 +0900

    nodeInfoChange

--- nodeControlServer/src/main/java/node/cluster/NodeInfoChangeEvent.java
+++ nodeControlServer/src/main/java/node/cluster/NodeInfoChangeEvent.java
@@ -1,10 +1,17 @@
 package node.cluster;
 
+import node.device.DeviceInfo;
+
 public class NodeInfoChangeEvent {
 	public static final int DEVICE_CONNECTED = 0;
 	public static final int DEVICE_DISCONNECTED = 1;
 	
 	public final DeviceInfo deviceInfo;
+	
+	public NodeInfoChangeEvent(DeviceInfo deviceInfo)
+	{
+		this.deviceInfo = deviceInfo;
+	}
 	
 	
 }
