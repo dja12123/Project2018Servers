9fb041b127ed6166f37b6a29fc97138f6f728c2f
LyntheYtoo <cdwdong1@gmail.com>
2018-10-08 10:50:52 +0900

    Update nodeControlServer/src/main/java/node/cluster/BashSet.java

--- nodeControlServer/src/main/java/node/cluster/BashSet.java
+++ nodeControlServer/src/main/java/node/cluster/BashSet.java
@@ -13,12 +13,20 @@ public class BashSet {
 	public static final File stop_spkWorker = FileHandler.getResourceFile("extResources/Shscript/stop_spkWorker.sh");
 	public static final File start_zkServer = FileHandler.getResourceFile("extResources/Shscript/start_zkServer.sh");
 	public static final File stop_zkServer = FileHandler.getResourceFile("extResources/Shscript/stop_zkServer.sh");
+	public static final File install_spark = FileHandler.getResourceFile("extResources/Shscript/install_spark.sh");
+	public static final File install_zookeeper = FileHandler.getResourceFile("extResources/Shscript/install_zookeeper.sh");
 	
-	public static void execSh(File shFile) {
+	//파일상수, 매개변수(옵션)1, 매개변수(옵션)2,... 이런식으로 사용
+	public static void execSh(File shFile, String... arg) {
 		//현재 노드에서 주키퍼 서버 실행
 		ArrayList<String> tempSh = new ArrayList<String>();
 		
-		tempSh.add(shFile.getAbsolutePath());
+		StringBuffer cmdline = new StringBuffer(shFile.getAbsolutePath());
+		for(int i = 0; i < arg.length; i++) {
+			cmdline.append(" " + arg[i]);
+		}
+		
+		tempSh.add(cmdline.toString());
 		
 		try {
 			CommandExecutor.executeBash(tempSh);
