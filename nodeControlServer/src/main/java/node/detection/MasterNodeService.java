5de8251b5744ef28740a3e7f91247b22bd5bd303
dja12123 <dja12123@dja-univ-main>
2018-10-27 22:40:39 +0900

    디버그삭제....

--- nodeControlServer/src/main/java/node/detection/MasterNodeService.java
+++ nodeControlServer/src/main/java/node/detection/MasterNodeService.java
@@ -155,18 +155,13 @@ public class MasterNodeService implements Runnable
 	{
 		if(data.getState(DeviceChangeEvent.DISCONNECT_DEVICE))
 		{
-			System.out.println("끊김1");
-			System.out.println("끊김2");
 			InetAddress deviceInetAddr = data.device.getInetAddr();
-			System.out.println("끊김3");
 			if(deviceInetAddr != null)
 			{
 				logger.log(Level.INFO, String.format("IP할당 해제 (%s)", data.device.uuid.toString()));
 				this.ipManager.removeInetAddr(data.device.uuid);
 			}
-			System.out.println("끊김4");
 			logger.log(Level.INFO, String.format("노드 연결 끊김  (%s)", data.device.uuid));
-			System.out.println("끊김5");
 		}
 	}
 
