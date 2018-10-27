1834a5c1098c58775cdad4c0cfc4320303cdd557
dja12123 <dja12123@dja-univ-main>
2018-10-27 21:50:07 +0900

    test

--- nodeControlServer/src/main/java/node/device/DeviceInfoManager.java
+++ nodeControlServer/src/main/java/node/device/DeviceInfoManager.java
@@ -186,7 +186,7 @@ public class DeviceInfoManager extends Observable<DeviceChangeEvent> implements 
 		{
 			synchronized (this)
 			{
-				compareTime = new Date(System.currentTimeMillis() + TIMEOUT);
+				compareTime = new Date(System.currentTimeMillis() - TIMEOUT);
 				removeDevices.clear();
 				
 				for(Device device : this.getDevices())
@@ -195,7 +195,7 @@ public class DeviceInfoManager extends Observable<DeviceChangeEvent> implements 
 					{
 						continue;
 					}
-					logger.log(Level.INFO, "노드 사망 감지" + device.uuid + " " + device.updateTime.toString() + " "+  compareTime.compareTo(device.updateTime));
+					logger.log(Level.INFO, "노드 사망 감지" + device.uuid + " " + device.updateTime.toString() + " "+  compareTime.before(device.updateTime));
 					if(device.updateTime.after(compareTime))
 					{//타임아웃일때
 						removeDevices.add(device);
