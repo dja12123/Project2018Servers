26668b444b1a7eb629a13732a477b704c77a602f
dja12123 <dja12123@dja-univ-main>
2018-10-27 21:39:47 +0900

    debug

--- nodeControlServer/src/main/java/node/device/DeviceInfoManager.java
+++ nodeControlServer/src/main/java/node/device/DeviceInfoManager.java
@@ -195,10 +195,11 @@ public class DeviceInfoManager extends Observable<DeviceChangeEvent> implements 
 					{
 						continue;
 					}
+					logger.log(Level.INFO, "노드 사망 감지" + device.uuid + " " + device.updateTime.toString());
 					if(device.getUpdateTime().after(compareTime))
 					{//타임아웃일때
 						removeDevices.add(device);
-						logger.log(Level.INFO, "노드 사망" + device.uuid);
+						
 					}
 				}
 				
