fc30dd42910866c287754c46c8af9964c06596e4
dja12123 <dja12123@dja-univ-main>
2018-10-27 21:37:47 +0900

    매우 치명적인 버그 픽스

--- nodeControlServer/src/main/java/node/device/DeviceChangeEvent.java
+++ nodeControlServer/src/main/java/node/device/DeviceChangeEvent.java
@@ -2,11 +2,11 @@ package node.device;
 
 public class DeviceChangeEvent
 {
-	public static final int CONNECT_NEW_DEVICE = 0b01;
-	public static final int DISCONNECT_DEVICE = 0b001;
-	public static final int CHANGE_INETADDR = 0b0001;
-	public static final int IS_MASTER_NODE = 0b00001;
-	public static final int IS_NOT_MASTER_NODE = 0b000001;
+	public static final int CONNECT_NEW_DEVICE = 0b1;
+	public static final int DISCONNECT_DEVICE = 0b10;
+	public static final int CHANGE_INETADDR = 0b100;
+	public static final int IS_MASTER_NODE = 0b10000;
+	public static final int IS_NOT_MASTER_NODE = 0b100000;
 	
 	private final int state;
 	public final Device device;
