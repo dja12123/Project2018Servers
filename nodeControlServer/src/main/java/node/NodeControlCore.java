23f661fdc0fdb23209dd8a7ac39ad8cffb521411
dja12123 <dja12123@dja-univ-main>
2018-10-27 17:18:25 +0900

    test

--- nodeControlServer/src/main/java/node/NodeControlCore.java
+++ nodeControlServer/src/main/java/node/NodeControlCore.java
@@ -80,7 +80,7 @@ public class NodeControlCore
 	{
 		try
 		{
-			//if(!this.dbHandler.startModule()) throw new Exception("DB핸들러 로드 실패");
+			if(!this.dbHandler.startModule()) throw new Exception("DB핸들러 로드 실패");
 			if(!this.deviceInfoManager.startModule()) throw new Exception("장치 정보 모듈 로드 실패");
 			if(!this.nodeDetectionService.startModule()) throw new Exception("노드 감지 서비스 모듈 로드 실패");
 			//if(!this.clusterService.startModule()) throw new Exception("스파크 모듈 로드 실패");
