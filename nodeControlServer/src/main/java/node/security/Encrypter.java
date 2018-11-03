7cf6e46b32f31daeb1a1c2260c3e3be94d986491
MelloRin <rpavpdlssla@naver.com>
2018-11-03 00:51:32 +0000

    temp

---
+++ nodeControlServer/src/main/java/node/security/Encrypter.java
@@ -0,0 +1,15 @@
+package node.security;
+
+import java.utill.Scanner;
+
+public class Encrypter
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        
+    }
+}
--- nodeControlServer/src/main/java/node/security/RSAEncrypt.java
+++ nodeControlServer/src/main/java/node/security/RSAEncrypt.java
@@ -5,26 +5,25 @@ import javax.crypto.Cipher;
 
 public class RSAEncrypt 
 {
-	public static int RSA_ENCRYPT_BYTE_SIZE = 512;
+	public static int RSA_ENCRYPT_BYTE_SIZE = 1024;
 	
-    public static byte[] incode(String inputStr, Key publicKey) throws Exception 
+    public static byte[] incode(byte[] target, Key publicKey) throws Exception 
     {
         Cipher cipher = Cipher.getInstance("RSA");
 
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] arrCipherData = cipher.doFinal(inputStr.getBytes());
+        byte[] arrCipherData = cipher.doFinal(target);
 
         return arrCipherData;
     }
     
-    public static String decode(byte[] arrCipherData, Key privateKey) throws Exception 
+    public static byte[] decode(byte[] target, Key privateKey) throws Exception 
     {
         Cipher cipher = Cipher.getInstance("RSA");
 
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] arrData = cipher.doFinal(arrCipherData);
-        String strResult = new String(arrData);
-
-        return strResult;
+        byte[] data = cipher.doFinal(target);
+            
+        return data;
     }
 }
