596e8c08ca1ce6edc4ee4f010d1ed9150804b177
dja12123 <dja12123@dja-univ-main>
2018-11-11 19:07:41 +0900

    test

--- nodeControlServer/src/main/java/node/NodeControlCore.java
+++ nodeControlServer/src/main/java/node/NodeControlCore.java
@@ -94,10 +94,8 @@ public class NodeControlCore
 		//환경 변수 설정 부분
 		try
 		{
-			cmdresult = CommandExecutor.executeCommand("readlink -f /usr/bin/javac");
-			System.out.println("DEBUG: " + cmdresult);
 			cmdresult = CommandExecutor.executeCommand("echo $JAVA_HOME");
-			if(cmdresult.equals(""))
+			if(cmdresult.equals(" "))
 			{// 환경 변수가 설정되지 않았을경우
 				logger.log(Level.INFO, "환경변수(JAVA_HOME) 설정");
 				cmdresult = CommandExecutor.executeCommand("readlink -f /usr/bin/javac");
