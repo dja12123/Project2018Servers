3afb22649165774160b0e889782d48725535a720
dja12123 <dja12123@dja-home-main>
2018-11-11 23:47:48 +0900

    test

--- nodeControlServer/src/main/java/node/network/socketHandler/RawSocketReceiver.java
+++ nodeControlServer/src/main/java/node/network/socketHandler/RawSocketReceiver.java
@@ -57,7 +57,7 @@ public class RawSocketReceiver implements Runnable
 			this.nic = NodeControlCore.getProp(NetworkManager.PROP_INTERFACE);
 			logger.log(Level.INFO, String.format("바인딩 인터페이스 (%s)", this.nic));
 			//String interfaceStr = NodeControlCore.getProp(NetworkManager.PROP_INTERFACE);
-			this.rawSocket.open(RawSocket.PF_INET, 0);
+			this.rawSocket.open(RawSocket.PF_INET, RawSocket.getProtocolByName("ICMP"));
 			this.rawSocket.bindDevice(this.nic);
 			
 			//NetworkUtil.getNetworkInterface(interfaceStr);
