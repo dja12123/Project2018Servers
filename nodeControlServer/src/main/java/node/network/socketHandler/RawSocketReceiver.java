586dbd35d71215037c1c7bd193ceecb8e30341b3
dja12123 <dja12123@dja-home-main>
2018-11-12 23:48:17 +0900

    test

--- nodeControlServer/src/main/java/node/network/socketHandler/RawSocketReceiver.java
+++ nodeControlServer/src/main/java/node/network/socketHandler/RawSocketReceiver.java
@@ -76,9 +76,9 @@ public class RawSocketReceiver implements Runnable
 			//logger.log(Level.INFO, String.format("바인딩 인터페이스 (%s)", this.nic));
 			//String interfaceStr = NodeControlCore.getProp(NetworkManager.PROP_INTERFACE);
 			this.rawSocket.open(RawSocket.PF_INET, RawSocket.getProtocolByName("UDP"));
-			String bindNIC = NetworkManager.getNIC()+":0";
-			logger.log(Level.INFO, String.format("바인드:(%s)", bindNIC));
-			this.rawSocket.bindDevice(bindNIC);
+			//String bindNIC = NetworkManager.getNIC()+":0";
+			//logger.log(Level.INFO, String.format("바인드:(%s)", bindNIC));
+			//this.rawSocket.bindDevice(bindNIC);
 			
 			//this.rawSocket.bindDevice(this.nic);
 			//this.rawSocket.setIPHeaderInclude(true);
