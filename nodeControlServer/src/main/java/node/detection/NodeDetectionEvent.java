e7c6c73d2c15716b8980cb75317773097ba9c946
dja12123 <dja12123@dja-univ-main>
2018-11-13 21:57:28 +0900

    주석달음

--- nodeControlServer/src/main/java/node/detection/NodeDetectionEvent.java
+++ nodeControlServer/src/main/java/node/detection/NodeDetectionEvent.java
@@ -7,8 +7,11 @@ public class NodeDetectionEvent {
 	public static final int STATE_FAIL = 1;
 	
 	public final InetAddress masterIP;
+	// 마스터 노드 아이피 (link fail일경우 null)
 	public final boolean isMaster;
+	// 내 가 마스터인지 아닌지
 	public final int state;
+	// 링크 상태
 	
 	public NodeDetectionEvent(InetAddress masterIP, boolean isMaster, int what) {
 		this.masterIP = masterIP;
