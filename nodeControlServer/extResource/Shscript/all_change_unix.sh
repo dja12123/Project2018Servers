245e0530fecd99ce0af23c64ea0db496e84d76ba
LyntheYtoo <cdwdong1@gmail.com>
2018-11-13 13:16:10 +0000

    yes

--- nodeControlServer/extResource/Shscript/all_change_unix.sh
+++ nodeControlServer/extResource/Shscript/all_change_unix.sh
@@ -1,4 +1,4 @@
 sudo apt-get install dos2unix
 
-dos2unix ~/Project2018Servers/bild/libs/extResource/Shscript/*
+dos2unix ~/Project2018Servers/build/libs/extResource/Shscript/*
 dos2unix ~/Project2018Servers/nodeControlServer/extResource/Shscript/*
