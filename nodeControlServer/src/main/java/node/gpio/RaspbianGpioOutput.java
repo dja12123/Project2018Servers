3f096a10bd14fd9f6917ed24eb8816983a41052d
kuronumast <wjdqkf1@gmail.com>
2018-11-13 21:54:04 +0900

    UpdatedGPIOControl

--- nodeControlServer/src/main/java/node/gpio/RaspbianGpioOutput.java
+++ nodeControlServer/src/main/java/node/gpio/RaspbianGpioOutput.java
@@ -25,17 +25,29 @@ public class RaspbianGpioOutput {
 
         final GpioController gpio = GpioFactory.getInstance();
 
-        Pin pin = CommandArgumentParser.getPin(
+        Pin pin7 = CommandArgumentParser.getPin(
                 RaspiPin.class,    // pin provider class to obtain pin instance from
                 RaspiPin.GPIO_07,  // default pin if no pin argument found
                 args);             // argument array to search in
+        Pin pin8 = CommandArgumentParser.getPin(
+                RaspiPin.class,    // pin provider class to obtain pin instance from
+                RaspiPin.GPIO_08,  // default pin if no pin argument found
+                args);             // argument array to search in
+        Pin pin9 = CommandArgumentParser.getPin(
+                RaspiPin.class,    // pin provider class to obtain pin instance from
+                RaspiPin.GPIO_09,  // default pin if no pin argument found
+                args);             // argument array to search in
 
         
-        final GpioPinDigitalOutput output = gpio.provisionDigitalOutputPin(pin, "GPIO Output", PinState.HIGH);
+        final GpioPinDigitalOutput output_pin7 = gpio.provisionDigitalOutputPin(pin7, "GPIO Output", PinState.LOW);
+        final GpioPinDigitalOutput output_pin8 = gpio.provisionDigitalOutputPin(pin8, "GPIO Output", PinState.LOW);
+        final GpioPinDigitalOutput output_pin9 = gpio.provisionDigitalOutputPin(pin9, "GPIO Output", PinState.LOW);
 
-        output.setShutdownOptions(false, PinState.LOW);
+        output_pin7.setShutdownOptions(true, PinState.LOW);
+        output_pin8.setShutdownOptions(true, PinState.LOW);
+        output_pin9.setShutdownOptions(true, PinState.LOW);
 
-        output.addListener(new GpioPinListenerDigital() {
+        output_pin7.addListener(new GpioPinListenerDigital() {
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                console.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " +
                         ConsoleColor.conditional(
@@ -46,51 +58,50 @@ public class RaspbianGpioOutput {
             }
         });
 
-        console.println(" ... Successfully provisioned output pin: " + output.toString());
+        console.println(" ... Successfully provisioned output pin: " + output_pin7.toString());
         console.emptyLine();
         console.box("The GPIO output pin states will cycle HIGH and LOW states now.");
         console.emptyLine();
 
-        console.println("--> [" + output.toString() + "] state was provisioned with state = " +
+        console.println("--> [" + output_pin7.toString() + "] state was provisioned with state = " +
                 ConsoleColor.conditional(
-                        output.getState().isHigh(), // conditional expression
+                		output_pin7.getState().isHigh(), // conditional expression
                         ConsoleColor.WHITE,         // positive conditional color
                         ConsoleColor.RED,           // negative conditional color
-                        output.getState()));        // text to display
+                        output_pin7.getState()));        // text to display
 
         Thread.sleep(500);
 
         console.emptyLine();
         console.println("Setting output pin state is set to LOW.");
-        output.low(); // or ... output.setState(PinState.LOW);
+        output_pin7.low(); // or ... output.setState(PinState.LOW);
 
         Thread.sleep(500);
 
         console.emptyLine();
         console.println("Setting output pin state from LOW to HIGH.");
-        output.setState(PinState.HIGH); // or ... output.high();
+        output_pin7.setState(PinState.HIGH); // or ... output.high();
 
         Thread.sleep(500);
 
         console.emptyLine();
         console.println("Toggling output pin state from HIGH to LOW.");
-        output.toggle();
+        output_pin7.toggle();
 
         Thread.sleep(500);
 
         console.emptyLine();
         console.println("Pulsing output pin state HIGH for 1 second.");
-        output.pulse(1000, true); // set second argument to 'true' use a blocking call
+        output_pin7.pulse(1000, true); // set second argument to 'true' use a blocking call
         Thread.sleep(50);
 
         console.emptyLine();
         console.println("Blinking output pin state between HIGH and LOW for 3 seconds with a blink rate of 250ms.");
-        Future future = output.blink(250, 3000);
+        Future future = output_pin7.blink(250, 3000);
 
         while(!future.isDone()){
             Thread.sleep(50);
         }
-        gpio.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);;
         gpio.shutdown();
     }
 }
