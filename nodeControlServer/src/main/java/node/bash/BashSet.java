0a74cf78d46d2e9fd66df43bc75931fa2d02cf7d
LyntheYtoo <cdwdong1@gmail.com>
2018-11-16 12:49:43 +0900

    update

--- nodeControlServer/src/main/java/node/bash/BashSet.java
+++ nodeControlServer/src/main/java/node/bash/BashSet.java
@@ -7,11 +7,11 @@ import node.fileIO.FileHandler;
 
 public class BashSet {
 	//sh 스크립트 추가 하실때 여기에 파일 상수 하나 추가해서 쓰세요
-	public static final File start_spkMaster = FileHandler.getExtResourceFile("$SPARK_HOME/sbin/start-master.sh");
-	public static final File stop_spkMaster = FileHandler.getExtResourceFile("$SPARK_HOME/sbin/stop-master.sh");
-	public static final File start_spkWorker = FileHandler.getExtResourceFile("$SPARK_HOME/sbin/start-slave.sh");
-	public static final File stop_spkWorker = FileHandler.getExtResourceFile("$SPARK_HOME/sbin/stop-master.sh");
-	public static final File install_spark = FileHandler.getExtResourceFile("$SPARK_HOME/sbin/install_spark.sh");
+	public static final File start_spkMaster = FileHandler.getExtResourceFile("Shscript/start_spkMaster.sh");
+	public static final File stop_spkMaster = FileHandler.getExtResourceFile("Shscript/stop_spkMaster.sh");
+	public static final File start_spkWorker = FileHandler.getExtResourceFile("Shscript/start_spkWorker.sh");
+	public static final File stop_spkWorker = FileHandler.getExtResourceFile("Shscript/stop_spkWorker.sh");
+	public static final File install_spark = FileHandler.getExtResourceFile("Shscript/install_spark.sh");
 	public static final File all_change_unix = FileHandler.getExtResourceFile("Shscript/all_change_unix.sh");
 	
 	//파일상수, 매개변수(옵션)1, 매개변수(옵션)2,... 이런식으로 사용
