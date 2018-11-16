dea896f818c0ced9bc6d555bdd42a0fe2788e0f1
LyntheYtoo <cdwdong1@gmail.com>
2018-11-16 14:09:48 +0900

    upload

--- nodeControlServer/extResource/Shscript/start_spkMaster.sh
+++ nodeControlServer/extResource/Shscript/start_spkMaster.sh
@@ -2,4 +2,4 @@ if [ "$#" -le 1 ]; then
 	exit 1
 fi
 
-$SPARK_HOME/sbin/start-master.sh "$@"
+$SPARK_HOME/sbin/start-master.sh $@
--- nodeControlServer/extResource/Shscript/start_spkWorker.sh
+++ nodeControlServer/extResource/Shscript/start_spkWorker.sh
@@ -2,4 +2,4 @@ if [ "$#" -le 1 ]; then
 	exit 1
 fi
 
-$SPARK_HOME/sbin/start-slave.sh "$@" #example spark://worker-11:7077 -m 512M -c 2
+$SPARK_HOME/sbin/start-slave.sh $@ #example spark://worker-11:7077 -m 512M -c 2
