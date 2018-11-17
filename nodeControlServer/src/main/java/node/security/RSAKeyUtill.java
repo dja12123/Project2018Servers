28a80198bfb1ec1d7e4ce15c679a65958391e4c8
MelloRin <rpavpdlssla@naver.com>
2018-11-17 10:55:37 +0900

    default RSAKey update

--- nodeControlServer/src/main/java/node/security/RSAKeyUtill.java
+++ nodeControlServer/src/main/java/node/security/RSAKeyUtill.java
@@ -4,9 +4,54 @@ import java.security.Key;
 import java.security.KeyFactory;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.Properties;
+
+import node.fileIO.FileHandler;
 
 public class RSAKeyUtill 
 {
+	public static final String DEFAULT_PUBLIC_KEY_PROP = "defaultPublicKey";
+	public static final String DEFAULT_PRIVATE_KEY_PROP = "defaultPrivateKey";
+	
+	public static void main(String[] args)
+	{
+		try 
+		{
+			Properties prop = new Properties();
+			prop.load(FileHandler.getResourceAsStream("/config.properties"));
+			
+			String[] publicKeyStrArr = prop.getProperty(DEFAULT_PUBLIC_KEY_PROP).split(" ");
+			byte[] defaultPublicKeyByte = new byte[publicKeyStrArr.length];
+			
+			for(int i = 0; i < publicKeyStrArr.length; ++i)
+				defaultPublicKeyByte[i] = (byte)Integer.parseInt(publicKeyStrArr[i], 16);
+				
+			
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			Key publicKey = kf.generatePublic(new X509EncodedKeySpec(defaultPublicKeyByte));
+			
+	
+			/*byte[] temp = RSAKeyManager.getInstance().getPublicKey().getEncoded();
+			for(byte value : temp)
+				System.out.printf("%02x ",value);
+			
+			System.out.println();
+			System.out.println("-------------------------------------------------------");
+            
+			temp = RSAKeyManager.getInstance().getPrivateKey().getEncoded();
+			for(byte value : temp)
+				System.out.printf("%02x ",value);*/
+			
+            //PKCS8EncodedKeySpec();
+		} 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public static String convertKeyToB64(Key key)
     {
         return Base64.getEncoder().encodeToString(key.getEncoded());
