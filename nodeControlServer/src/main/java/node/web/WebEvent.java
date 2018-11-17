b66d668b13da052149994ab4fb97be61c7c8e785
Marlgolro <mrtj0329@gmail.com>
2018-11-17 18:11:14 +0900

    웹이벤트, 소켓 옵저버

---
+++ nodeControlServer/src/main/java/node/web/WebEvent.java
@@ -0,0 +1,13 @@
+package node.web;
+
+//channel
+
+public class WebEvent {
+	public final String key;
+	public final String value;
+	
+	WebEvent(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}	
+}
