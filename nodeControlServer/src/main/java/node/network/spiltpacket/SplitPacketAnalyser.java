39469a1e3a88ffb1c746cfb3ef7b3a5032200a48
dja12123 <dja12123@172.17.27.171>
2018-11-17 17:40:21 +0900

    splitPacket 프로토콜 제작중

--- nodeControlServer/src/main/java/node/network/spiltpacket/SplitPacketAnalyser.java
+++ nodeControlServer/src/main/java/node/network/spiltpacket/SplitPacketAnalyser.java
@@ -2,25 +2,59 @@ package node.network.spiltpacket;
 // 현재 splitPacket 프로토콜은 UDP패킷을 수신할 때 IP스푸핑을 사용한 응용 레벨 서비스 거부 공격에 대응할 수 없습니다. 개선 필요
 
 import java.net.InetAddress;
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import node.log.LogWriter;
+import node.network.NetworkUtil;
 
 public class SplitPacketAnalyser
 {
-	private final int SEGMENT_TIMEOUT = 100;
-	private final int CHECK_DELAY = 1000;
+	public static final Logger logger = LogWriter.createLogger(SplitPacketAnalyser.class, "splitPacket");
+	
+	private static final int SEGMENT_TIMEOUT = 100;
+	private static final int CHECK_DELAY = 1000;
 	
 	private Thread worker;
 	private boolean isRun;
 	
-	private HashMap<InetAddress, PacketQueue> packetStack;
-	private HashMap<InetAddress, SplitPacketBuilder> builderStack;
+	private HashMap<InetAddress, PacketQueue> ipQueueStack;
+	private HashMap<Long, SplitPacketBuilder> builderStack;
+	
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("시작");
+		InetAddress addr1 = InetAddress.getByName("192.168.0.1");
+		InetAddress addr2 = InetAddress.getByName("192.168.0.1");
+		InetAddress addr3 = InetAddress.getByName("192.168.0.1");
+		
+		byte[] test = new byte[1024];
+		for(int i = 0; i < test.length; ++i)
+		{
+			test[i] = 0x1F;
+		}
+		
+		SplitPacket p = new SplitPacket(SplitPacketUtil.createSplitPacketID(addr1), test);
+		System.out.println(p.segCount);
+		
+		for(int i = 0; i < p.segCount; ++i)
+		{
+			byte[] seg = p.getSegment(i);
+			System.out.println(NetworkUtil.bytesToHex(seg, seg.length));
+		}
+		SplitPacketAnalyser analyser = new SplitPacketAnalyser();
+		analyser.start();
+		//analyser.analysePacket(addr1, test);
+	}
 	
 	public SplitPacketAnalyser()
 	{
 		this.worker = null;
 		this.isRun = false;
-		this.packetStack = new HashMap<>();
+		this.ipQueueStack = new HashMap<>();
 		this.builderStack = new HashMap<>();
 	}
 	
@@ -41,40 +75,112 @@ public class SplitPacketAnalyser
 		this.isRun = false;
 		
 		this.worker.interrupt();
-		this.packetStack.clear();
+		this.ipQueueStack.clear();
 		this.builderStack.clear();
 	}
 	
 	public synchronized void analysePacket(InetAddress inetAddr, byte[] copyedBuffer)
 	{// 패킷 분석해서 스택에 넣어줌...
-		ByteBuffer orgBuffer = ByteBuffer.wrap(copyedBuffer);
-		PacketQueue queue = this.packetStack.get(inetAddr);
-		
+		PacketQueue queue = this.ipQueueStack.get(inetAddr);
 		if(queue == null)
 		{
-			queue = new PacketQueue(SplitPacketUtil.FULL_PACKET_LIMIT);
-			this.packetStack.put(inetAddr, queue);
+			queue = new PacketQueue(SplitPacketUtil.SPLIT_SIZE);
+			this.ipQueueStack.put(inetAddr, queue);
 		}
+		queue.updateTime();
 		
 		for(int i = 0; i < copyedBuffer.length; ++i)
 		{
+			System.out.println(queue);
 			if(queue.enQueue(copyedBuffer[i]))
 			{
-				System.out.println("패킷이당");
+				System.out.println("감지완료");
+				if(queue.getSize() > SplitPacketUtil.PACKET_METADATA_SIZE)
+				{
+					this.processPacket(queue.getSnapShot());
+				}
 			}
 		}
-		
+	}
+	
+	private void processPacket(byte[] snapShot)
+	{
+		long id = SplitPacketUtil.headerToLong(snapShot);
+		System.out.println("패킷이당");
+		SplitPacketBuilder builder = this.builderStack.get(id);
+		try
+		{
+			if(builder == null)
+			{
+				builder = new SplitPacketBuilder();
+				this.builderStack.put(id, builder);
+				builder.setID(id);
+				System.out.println("빌더 생성");
+			}
+			if(!builder.checkPacket(snapShot))
+			{
+				this.builderStack.remove(id);
+				System.out.println("오류 빌더 제거");
+				return;
+			}
+			System.out.println("빌더에 패킷 추가");
+			builder.addRawPacket(snapShot);
+			if(builder.isBuilded())
+			{
+				this.builderStack.remove(id);
+				System.out.println("빌드 완성!");
+				return;
+			}
+			builder.updateTime();
+		}
+		catch (SplitPacketBuildFailureException e)
+		{
+			logger.log(Level.WARNING, "패킷 처리중 오류", e);
+		}
 	}
 	
 	private void run()
 	{
+		Date compareTime;
+		ArrayList<InetAddress> removeQueue = new ArrayList<>();
+		ArrayList<Long> removeBuilders = new ArrayList<>();
 		while(this.isRun)
 		{
+			compareTime = new Date(System.currentTimeMillis() - SEGMENT_TIMEOUT);
+			removeQueue.clear();
+			removeBuilders.clear();
+			
 			synchronized (this)
 			{
-				for(SplitPacketBuilder builder : this.builderStack.values())
+				for(InetAddress key : this.ipQueueStack.keySet())
 				{
-					
+					PacketQueue queue = this.ipQueueStack.get(key);
+					if(compareTime.after(queue.getTime()))
+					{//타임아웃일때
+						removeQueue.add(key);
+					}
+				}
+				for(int i = 0; i < removeQueue.size(); ++i)
+				{
+					this.ipQueueStack.remove(removeQueue.get(i));
+					System.out.println("IP타임아웃이당");
+				}
+			}
+			
+			synchronized (this)
+			{
+				for(long key : this.builderStack.keySet())
+				{
+					SplitPacketBuilder builder = this.builderStack.get(key);
+					if(compareTime.after(builder.getTime()))
+					{//타임아웃일때
+						removeBuilders.add(key);
+					}
+				}
+				for(int i = 0; i < removeBuilders.size(); ++i)
+				{
+					this.builderStack.remove(removeBuilders.get(i));
+					System.out.println("빌더타임아웃이당");
 				}
 			}
 			
@@ -90,169 +196,3 @@ public class SplitPacketAnalyser
 	}
 }
 
-
-class PacketQueue
-{
-	private int front;
-	private int rear;
-	private final int queueSize;
-	private byte itemArray[];
-	
-	private boolean isStart;
-	private boolean isEnd;
-	private int size;
-	
-	private int isStartLife;
-	private int isEndLife;
-
-	@SuppressWarnings("unchecked")
-	public PacketQueue(int queueSize)
-	{
-		this.front = 0;
-		this.rear = 0;
-		this.queueSize = queueSize + 1;
-		this.itemArray = new byte[queueSize + 1];
-		
-		this.isStart = false;
-		this.isEnd = false;
-	}
-
-	// 큐가 비어있는지 확인
-	public boolean isEmpty()
-	{
-		return (front == rear);
-	}
-
-	// 큐가 가득차 있는지 확인
-	public boolean isFull()
-	{
-		return this.queueSize - 1 == this.size;
-	}
-
-	// 큐의 삽입 연산
-	public boolean enQueue(byte item)
-	{
-		if (this.isFull())
-		{
-			this.front = (this.front + 1) % this.queueSize;
-		}
-		
-		this.rear = (this.rear + 1) % (this.queueSize);
-		this.setSize();
-		this.itemArray[this.rear] = item;
-		
-		if(this.size >= SplitPacketUtil.MAGIC_NO_START.length)
-		{
-			if(this.findPattern((this.front + this.size - SplitPacketUtil.MAGIC_NO_START.length)
-					, SplitPacketUtil.MAGIC_NO_START))
-			{
-				System.out.println("스타트감지");
-				this.isStart = true;
-				this.isStartLife = this.queueSize - SplitPacketUtil.MAGIC_NO_START.length;
-			}
-			
-			if(this.findPattern((this.front + this.size - SplitPacketUtil.MAGIC_NO_END.length)
-					, SplitPacketUtil.MAGIC_NO_END))
-			{
-				
-				this.isEnd = true;
-				this.isEndLife = this.queueSize - SplitPacketUtil.MAGIC_NO_END.length;
-			}
-		}
-		
-		if(this.isStart)
-		{
-			--this.isStartLife;
-			if(this.isStartLife < 0)
-			{
-				this.isStart = false;
-			}
-		}
-		
-		if(this.isEnd)
-		{
-			--this.isEndLife;
-			if(this.isEndLife < 0)
-			{
-				this.isEnd = false;
-			}
-		}
-		
-		if(this.isStart && this.isEnd && this.isStartLife == 0 && this.isEndLife == this.queueSize - SplitPacketUtil.MAGIC_NO_END.length - 1)
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	public byte get(int index)
-	{
-		return this.itemArray[(index + this.front + 1) % this.queueSize];
-	}
-
-	// 큐의 현재 front값 출력
-	public byte peek()
-	{
-		if (this.isEmpty())
-		{
-			return 0;
-		}
-		else
-		{
-			return this.itemArray[(this.front + 1) % this.queueSize];
-		}
-	}
-	
-	// 전체 큐값 출력
-	@Override
-	public String toString()
-	{
-		if(this.isEmpty())
-		{
-			return "";
-		}
-		StringBuffer buf = new StringBuffer();
-		int f = this.front;
-
-		while (f != this.rear)
-		{
-			f = (f + 1) % this.queueSize;
-			buf.append(String.format("%2s ", Integer.toHexString(this.itemArray[f])));
-		}
-		buf.append('\n');
-		return buf.toString();
-	}
-	
-	private void setSize()
-	{
-		this.size = this.front > this.rear ? 
-				(this.queueSize - this.front + this.rear) : (this.rear - this.front);
-	}
-	
-	private boolean findPattern(int front, byte[] pattern)
-	{
-		int findTargetPointer = 0;
-		
-		for(int i = 0; i < pattern.length; ++i)
-		{
-			if(findTargetPointer == pattern.length)
-			{
-				return true;
-			}
-			if(this.itemArray[(i + front + 1) % this.queueSize] == pattern[findTargetPointer])
-			{
-				++findTargetPointer;
-			}
-			else
-			{
-				findTargetPointer = 0;
-			}
-		}
-		if(findTargetPointer == pattern.length)
-		{
-			return true;
-		}
-		return false;
-	}
-
-}
