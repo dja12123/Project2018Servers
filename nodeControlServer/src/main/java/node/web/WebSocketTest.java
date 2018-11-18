a474464f506df5901a498ea757c92d7a75112ac9
Marlgolro <mrtj0329@gmail.com>
2018-11-18 20:27:30 +0900

    소켓 테스트

--- nodeControlServer/src/main/java/node/web/WebSocketTest.java
+++ nodeControlServer/src/main/java/node/web/WebSocketTest.java
@@ -2,6 +2,19 @@ package node.web;
 
 import java.io.IOException;
 
+import org.nanohttpd.protocols.websockets.NanoWSD;
+
 public class WebSocketTest {
-	//소켓 테스트 관련 내용
+    public static void main(String[] args) throws IOException {
+        final boolean debugMode = args.length >= 2 && "-d".equals(args[1].toLowerCase());
+        NanoWSD ws = new WebSocketManager(args.length > 0 ? Integer.parseInt(args[0]) : 9090, debugMode);
+        ws.start();
+        System.out.println("Server started, hit Enter to stop.\n");
+        try {
+            System.in.read();
+        } catch (IOException ignored) {
+        }
+        ws.stop();
+        System.out.println("Server stopped.\n");
+    }
 }
