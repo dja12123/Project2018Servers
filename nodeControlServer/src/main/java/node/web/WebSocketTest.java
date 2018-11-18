d4917e2cbcbfbddbcb509f7002a89cb89f9b00df
Marlgolro <mrtj0329@gmail.com>
2018-11-18 19:56:21 +0900

    path 경로 수정

--- nodeControlServer/src/main/java/node/web/WebSocketTest.java
+++ nodeControlServer/src/main/java/node/web/WebSocketTest.java
@@ -2,9 +2,6 @@ package node.web;
 
 import java.io.IOException;
 
-import org.nanohttpd.protocols.websockets.NanoWSD;
-import org.nanohttpd.samples.websockets.DebugWebSocketServer;
-
 public class WebSocketTest {
 	//소켓 테스트 관련 내용
 }
