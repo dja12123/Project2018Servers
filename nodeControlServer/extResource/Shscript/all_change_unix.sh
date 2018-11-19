fb4074ae77becc56b495419975949f264460abf5
LyntheYtoo <cdwdong1@gmail.com>
2018-11-19 11:44:02 +0000

    dsfds

--- nodeControlServer/extResource/Shscript/all_change_unix.sh
+++ nodeControlServer/extResource/Shscript/all_change_unix.sh
@@ -4,3 +4,5 @@ dos2unix ~/Project2018Servers/build/libs/extResource/Shscript/*
 dos2unix ~/Project2018Servers/nodeControlServer/extResource/Shscript/*
 
 chmod 755 ~/Project2018Servers/build/libs/extResource/Shscript/*
+chmod 755 ~/Project2018Servers/nodeControlServer/extResource/Shscript/*
+
