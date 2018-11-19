12c2794f595a4fd1d32acdcba785a157d3ee6814
LyntheYtoo <cdwdong1@gmail.com>
2018-11-19 10:31:16 +0900

    Create uninstall_spark.sh

---
+++ nodeControlServer/extResource/Shscript/uninstall_spark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "need input argument"
+	exit 1
+fi
+
+sed -i "/export SPARK_HOME=$1/spark/d" /etc/bash.bashrc
+sed -i '/export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/jre/d' /etc/bash.bashrc
+sed -i '/export PATH=$PATH:$SPARK_HOME/bin/d' /etc/bash.bashrc
+
+rm -r $1/spark
+
+echo "Reboot System?"
+read an
+if [ an = "y" | and = "yes" ]; then
+	shutdown -r
+fi
