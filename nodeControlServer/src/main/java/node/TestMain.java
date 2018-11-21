ffa5e26522b35ed59724dc2c66a62189c18da96c
dja12123 <dja12123@dja-univ-main>
2018-11-21 22:06:00 +0900

    test

--- nodeControlServer/src/main/java/node/TestMain.java
+++ nodeControlServer/src/main/java/node/TestMain.java
@@ -181,7 +181,7 @@ public class TestMain implements Runnable
 		{
 			this.rawSocket.open(RawSocket.PF_INET, RawSocket.getProtocolByName("UDP"));
 			//this.rawSocket.setIPHeaderInclude(true);
-			//this.rawSocket.bindDevice(nic);
+			this.rawSocket.bindDevice(nic);
 			//logger.log(Level.INFO, String.format("바인드:(%s)", nic));
 		}
 		catch (IllegalStateException | IOException e)
