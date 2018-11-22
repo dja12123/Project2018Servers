02d4f80b0cbd64730af2c4d5e6e21d12ffe5fc90
dja12123 <dja12123@dja-univ-main>
2018-11-23 00:28:19 +0900

    test

--- nodeControlServer/src/main/java/node/gpio/led/LEDControl.java
+++ nodeControlServer/src/main/java/node/gpio/led/LEDControl.java
@@ -80,6 +80,7 @@ public class LEDControl
 		try (LedDriverInterface iface = new WS281xSpi(2, 0, StripType.WS2812, NUM_LED, 255))
 		{
 			this.ledDriver = iface;
+			System.out.println(Thread.currentThread().getName());
 
 			for (int i = 0; i < NUM_LED; ++i)
 			{
