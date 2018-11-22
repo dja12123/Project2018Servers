110d58afe96fca330e25484b49eaf2bc0b3c58f2
dja12123 <dja12123@dja-univ-main>
2018-11-23 01:16:19 +0900

    test

--- nodeControlServer/src/main/java/node/gpio/led/LEDControlInst.java
+++ nodeControlServer/src/main/java/node/gpio/led/LEDControlInst.java
@@ -65,7 +65,7 @@ public class LEDControlInst
 			}
 			else
 			{//led off
-				this.led_driver.setPixelColourRGB(this.pixel, this.br, this.bg, this.bb);
+				//this.led_driver.setPixelColourRGB(this.pixel, this.br, this.bg, this.bb);
 				//this.led_driver.render();
 				
 				this.isLight = false;
@@ -82,7 +82,7 @@ public class LEDControlInst
 			}
 			else
 			{// led on
-				this.led_driver.setPixelColourRGB(this.pixel, this.r, this.g, this.b);
+				//this.led_driver.setPixelColourRGB(this.pixel, this.r, this.g, this.b);
 				//this.led_driver.render();
 				
 				this.isLight = true;
@@ -103,7 +103,7 @@ public class LEDControlInst
 	{
 		if(this.isLight)
 		{
-			led_driver.setPixelColourRGB(this.pixel, this.r, this.g, this.b);
+			//led_driver.setPixelColourRGB(this.pixel, this.r, this.g, this.b);
 			//led_driver.render();
 			return true;
 		}
