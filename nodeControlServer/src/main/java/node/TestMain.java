8e646a32478f70fd21339481ed61c94e13a5568c
dja12123 <dja12123@dja-univ-main>
2018-11-23 07:49:06 +0900

    test

--- nodeControlServer/src/main/java/node/TestMain.java
+++ nodeControlServer/src/main/java/node/TestMain.java
@@ -3,6 +3,9 @@ package node;
 import java.awt.Color;
 import java.util.logging.Logger;
 
+import de.cacodaemon.rpiws28114j.StripType;
+import de.cacodaemon.rpiws28114j.WS2811;
+import de.cacodaemon.rpiws28114j.WS2811Channel;
 import de.pi3g.pi.ws2812.WS2812;
 public class TestMain
 {
@@ -12,6 +15,19 @@ public class TestMain
 		WS2812.get().clear();    
 		WS2812.get().setPixelColor(0, Color.RED); //sets the color of the fist LED to red
 		WS2812.get().show();
+		
+		WS2811.init(new WS2811Channel(
+	            10, // gpioPin
+	            4, //ledCount
+	            StripType.WS2811_STRIP_RGB,
+	            false, // invert
+	            128 // brightness
+	    ));
+	    
+
+		WS2811.setPixel(0, 0x00FF00);
+
+		WS2811.render();
 		Thread.sleep(10000);
 	}
 }
