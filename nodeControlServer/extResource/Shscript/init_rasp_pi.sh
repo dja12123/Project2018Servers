e89419c042df191c2bf509ff471a4d1c52019fb5
LyntheYtoo <cdwdong1@gmail.com>
2018-11-23 01:57:46 +0900

    Update init_rasp_pi.sh

--- nodeControlServer/extResource/Shscript/init_rasp_pi.sh
+++ nodeControlServer/extResource/Shscript/init_rasp_pi.sh
@@ -1,4 +1,6 @@
+sed -i 's/http:\/\/raspbian.raspberrypi.org\/raspbian\//http:\/\/mirror.premi.st\/raspbian\/raspbian\//g' /etc/apt/sources.list
+
 apt-get -y update
-#apt-get -y upgrade
+apt-get -y upgrade
 apt-get -y install gradle
 apt-get -y install default-jdk
