b9db3d17b92e8d29a23c89a830ff75e3822dcf4d
dja12123 <dja12123@dja-univ-main>
2018-11-23 02:16:14 +0900

    test

--- nodeControlServer/src/main/java/node/gpio/led/LEDControl.java
+++ nodeControlServer/src/main/java/node/gpio/led/LEDControl.java
@@ -26,7 +26,9 @@ public class LEDControl implements Runnable
 		System.out.println("시작");
 		this.ledDriver  = new WS281xSpi(2, 0, StripType.WS2812, NUM_LED, 200);
 		this.ledDriver.allOff();
-		this.ledDriver.setPixelColourRGB(0, 100, 100, 100);
+		this.ledDriver.setRedComponent(0, 100);
+		this.ledDriver.setBlueComponent(0, 100);
+		this.ledDriver.setGreenComponent(0, 100);
 		this.ledDriver.render();
 		SleepUtil.sleepMillis(1000);
 		this.ledDriver.setPixelColourRGB(0, 0, 0, 0);
