ea20d1cce20274f454ec4bc286aeb41b6f53531e
LyntheYtoo <cdwdong1@gmail.com>
2018-11-22 23:13:42 +0900

    Update ml_lib_install.sh

--- nodeControlServer/extResource/Shscript/ml_lib_install.sh
+++ nodeControlServer/extResource/Shscript/ml_lib_install.sh
@@ -1,8 +1,10 @@
-if [ dpkg -s python3 ]; then
+dpkg -s "python-pip" &> /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
 	echo "already installed"
 	exit 1
 fi
-yes | apt-get install python 3
+yes | apt-get install python3
 yes | apt-get install python-pip
 pip install tensorflow
 pip install tensorflowonspark
