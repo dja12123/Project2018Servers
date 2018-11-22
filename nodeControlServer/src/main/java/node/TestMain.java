99eb8344ee61075a494edf2e285c4d9abd865e74
dja12123 <dja12123@dja-univ-main>
2018-11-22 20:35:19 +0900

    test

--- nodeControlServer/src/main/java/node/TestMain.java
+++ nodeControlServer/src/main/java/node/TestMain.java
@@ -11,12 +11,13 @@ public class TestMain
 {
 	public static final Logger logger = LogWriter.createLogger(RawSocketReceiver.class, "rawsocket");
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		WS2812.get().init(4); //init a chain of 64 LEDs
 		WS2812.get().clear();    
 		WS2812.get().setPixelColor(0, Color.RED); //sets the color of the fist LED to red
 		WS2812.get().show();
+		Thread.sleep(5000);
 	}
 	
 
