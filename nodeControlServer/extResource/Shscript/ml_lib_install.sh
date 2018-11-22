9a61d0707c1824d76c3d74603dab33b6e0c42544
LyntheYtoo <cdwdong1@gmail.com>
2018-11-23 00:48:20 +0900

    Update ml_lib_install.sh

--- nodeControlServer/extResource/Shscript/ml_lib_install.sh
+++ nodeControlServer/extResource/Shscript/ml_lib_install.sh
@@ -1,15 +1,20 @@
 dpkg -s "python3-pip" > /dev/null 2>&1
 
+bashrc=/etc/bash.bashrc
+
+if ! grep -q ".*PYSPARK_PYTHON.*" $bashrc: then
+	echo "export PYSPARK_PYTHON=python3" >> $bashrc
+fi
+
 if [ $? -eq 0 ]; then
 	echo "already installed"
 	exit 1
 fi
-yes | apt-get update
+apt-get -y update
 
-yes | apt install libatlas-base-dev
+apt -y install libatlas-base-dev
 
-yes | apt-get install python3-picamera
-yes | apt-get install python3-pip
+apt-get -y install python3-pip
 
 pip3 install tensorflow
 pip3 install tensorflowonspark
