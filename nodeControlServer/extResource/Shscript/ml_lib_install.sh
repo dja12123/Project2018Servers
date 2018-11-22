2916bafdf9a4a6469db82e7863b03d17b4ad667a
LyntheYtoo <cdwdong1@gmail.com>
2018-11-23 02:08:39 +0900

    Update ml_lib_install.sh

--- nodeControlServer/extResource/Shscript/ml_lib_install.sh
+++ nodeControlServer/extResource/Shscript/ml_lib_install.sh
@@ -1,17 +1,3 @@
-dpkg -s "python3-pip" > /dev/null 2>&1
-
-bashrc=/etc/bash.bashrc
-
-if ! grep -q ".*PYSPARK_PYTHON.*" $bashrc; then
-	echo "export PYSPARK_PYTHON=python3" >> $bashrc
-fi
-
-. $bashrc
-
-if [ $? -eq 0 ]; then
-	echo "already installed"
-	exit 1
-fi
 apt-get -y update
 
 apt -y install libatlas-base-dev
