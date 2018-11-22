c9123c28fc6868a9ef0f17b583ccd4e0d09f5a32
dja12123 <dja12123@dja-univ-main>
2018-11-23 00:47:07 +0900

    test

--- nodeControlServer/src/main/java/node/gpio/led/LEDControlInst.java
+++ nodeControlServer/src/main/java/node/gpio/led/LEDControlInst.java
@@ -63,8 +63,8 @@ public class LEDControlInst
 			}
 			else
 			{//led off
-				//led_driver.setPixelColourRGB(this.pixel, this.br, this.bg, this.bb);
-				//led_driver.render();
+				led_driver.setPixelColourRGB(this.pixel, this.br, this.bg, this.bb);
+				led_driver.render();
 				
 				this.isLight = false;
 				this.time = this.blackTime;
