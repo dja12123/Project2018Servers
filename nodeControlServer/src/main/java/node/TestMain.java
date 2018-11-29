8b6f1a61f3291007633cd829bdb737d2929ba2f9
dja12123 <dja12123@dja-univ-main>
2018-11-29 19:39:21 +0900

    test

--- nodeControlServer/src/main/java/node/TestMain.java
+++ nodeControlServer/src/main/java/node/TestMain.java
@@ -37,7 +37,7 @@ public class TestMain
 	public static Font font;
 	public static void main(String[] args) throws IOException, ReflectiveOperationException, UnsupportedBusNumberException, InterruptedException, FontFormatException
 	{
-		font = Font.createFont(Font.TRUETYPE_FONT, TestMain.class.getResourceAsStream("font/D2Coding"));
+		font = Font.createFont(Font.TRUETYPE_FONT, TestMain.class.getResourceAsStream("font/D2Coding.ttf"));
 		OLEDDisplay display = new OLEDDisplay();
 		int i = 0;
 		while(true)
