5fa01700ef6ad2662203ed8117337eb639c97a7a
dja12123 <dja12123@dja-univ-main>
2018-11-29 21:10:44 +0900

    폰트12

--- nodeControlServer/src/main/java/node/TestMain.java
+++ nodeControlServer/src/main/java/node/TestMain.java
@@ -24,7 +24,7 @@ public class TestMain
 		
 
 		font = Font.createFont(Font.TRUETYPE_FONT, TestMain.class.getResourceAsStream("/font/D2Coding.ttf"));
-		font = font.deriveFont(Font.PLAIN, 14);
+		font = font.deriveFont(Font.PLAIN, 12);
 
 		OLEDDisplay display = new OLEDDisplay();
 		int x = 0;
