02ae710e3311f175846d6ef55b70e31b1090b6dd
dja12123 <dja12123@dja-univ-main>
2018-11-29 18:53:27 +0900

    test

--- nodeControlServer/src/main/java/node/TestMain.java
+++ nodeControlServer/src/main/java/node/TestMain.java
@@ -1,23 +1,25 @@
 package node;
 
+import java.awt.Font;
+import java.awt.Image;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
+
+import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import com.pi4j.io.spi.SpiChannel;
+import com.pi4j.io.spi.SpiFactory;
 
-import net.fauxpark.oled.Graphics;
-import net.fauxpark.oled.SSD1306;
-import net.fauxpark.oled.font.CodePage1252;
-import net.fauxpark.oled.impl.SSD1306I2CImpl;
-import net.fauxpark.oled.impl.SSD1306MockImpl;
-import net.fauxpark.oled.impl.SSD1306SPIImpl;
+import eu.ondryaso.ssd1306.Display;
+import eu.ondryaso.ssd1306.examples.BasicGraphics;
 import node.util.observer.Observable;
 import node.util.observer.Observer;
 import node.web.WebEvent;
@@ -25,25 +27,34 @@ import node.web.WebManager;
 
 public class TestMain
 {
-	public static void main(String[] args) throws InterruptedException, IOException, UnsupportedBusNumberException
+	public static void main(String[] args) throws IOException, ReflectiveOperationException, UnsupportedBusNumberException
 	{
-		SSD1306 ssd1306 = new SSD1306MockImpl(128, 64);
-		Graphics graphics = ssd1306.getGraphics();
+		Display disp = new Display(128, 64, GpioFactory.getInstance(), I2CFactory.getInstance(I2CBus.BUS_1), 0x3D);
 
-		ssd1306.startup(false);
-		
-		// Draws a line from the top left to the bottom right of the display
-		graphics.line(0, 0, 127, 63);
+		// Create 128x64 display on CE1 (change to SpiChannel.CS0 for using CE0) with
+		// D/C pin on WiringPi pin 04
 
-		// Draws an arc from (63,31) with a radius of 8 pixels and an angle of 15 degrees
-		graphics.arc(63, 31, 8, 0, 15);
+		disp.begin();
+		// Init the display
 
-		// Writes "Hello world!" at (20,20) using the Windows-1252 charset
-		graphics.text(20, 20, new CodePage1252(), "Hello world!");
-		ssd1306.display();
-		Thread.sleep(3000);
+		disp.getGraphics().setFont(new Font("Monospaced", Font.PLAIN, 10));
+		disp.getGraphics().drawString("Praise him", 64, 60);
+		disp.getGraphics().drawRect(0, 0, disp.getWidth() - 1, disp.getHeight() - 1);
+		// Deal with the image using AWT
 
-		
+		disp.displayImage();
+		// Copy AWT image to an inner buffer and send to the display
+
+		for (int x = 70; x < 90; x += 2)
+		{
+			for (int y = 10; y < 30; y += 2)
+			{
+				disp.setPixel(x, y, true);
+			}
+		}
+		// Set some pixels in the buffer manually
+
+		disp.display();
+		// Send the buffer to the display again, now with the modified pixels
 	}
-
 }
