03d2602232c82721bf216bf5939b51de5a2277a0
LyntheYtoo <cdwdong1@gmail.com>
2018-12-03 18:15:09 +0900

    Update init_rasp_pi.sh

--- nodeControlServer/extResource/Shscript/init_rasp_pi.sh
+++ nodeControlServer/extResource/Shscript/init_rasp_pi.sh
@@ -1,4 +1,4 @@
-#sed -i 's/http:\/\/raspbian.raspberrypi.org\/raspbian\//http:\/\/ftp.kaist.ac.kr\/raspbian\/raspbian\//g' /etc/apt/sources.list
+sed -i 's/http:\/\/raspbian.raspberrypi.org\/raspbian\//http:\/\/ftp.neowiz.com\/raspbian\/raspbian\//g' /etc/apt/sources.list
 
 apt-get -y update
 apt-get -y upgrade
