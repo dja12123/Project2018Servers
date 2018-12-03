115e0834dd6abf7eae3934ee2ea9a2c0d00a3e12
LyntheYtoo <cdwdong1@gmail.com>
2018-12-03 21:01:56 +0900

    Update SparkManager.java

--- nodeControlServer/src/main/java/node/cluster/spark/SparkManager.java
+++ nodeControlServer/src/main/java/node/cluster/spark/SparkManager.java
@@ -10,6 +10,7 @@ import node.bash.BashSet;
 import node.bash.CommandExecutor;
 import node.cluster.ClusterService;
 import node.log.LogWriter;
+import node.gpio.lcd.*;
 
 public class SparkManager {
 	private String sparkInstDir;
@@ -21,12 +22,16 @@ public class SparkManager {
 	private String sparkWorkerCores;
 	private String sparkWorkerMemory;
 	
+	private LCDObject lcdPan;
+	private int lcdX = 1, lcdY = 35, lcdW = 50;
+	
 	public static final Logger sparkLogger = LogWriter.createLogger(SparkManager.class, "spark");
 	
 	public SparkManager() {
 		
 	}
 	public void startSparkMaster(String masterIP, String option) {
+		lcdPan = LCDControl.inst.replaceString(lcdPan, "Spark 마스터 동작중");
 		sparkLogger.log(Level.INFO, "스파크 마스터 시작");
 		StringBuffer confOp = new StringBuffer("-h ")
 				.append(masterIP)
@@ -37,11 +42,14 @@ public class SparkManager {
 		BashSet.execSh(BashSet.start_spkMaster, confOp.toString(), option);
 	}
 	public void stopSparkMaster() {
+		lcdPan = LCDControl.inst.replaceString(lcdPan, "Spark 마스터 중지중");
+		lcdPan = LCDControl.inst.removeShapeTimer(lcdPan, 2000);
 		sparkLogger.log(Level.INFO, "스파크 마스터 중지");
 		BashSet.execSh(BashSet.stop_spkMaster);
 	}
 	//option example spark://worker-11:7077 -m 512M -c 2
 	public void startSparkWorker(String masterIp, String option) {
+		lcdPan = LCDControl.inst.replaceString(lcdPan, "Spark 워커 동작중");
 		sparkLogger.log(Level.INFO, "스파크 워커 시작");
 		StringBuffer confOp = new StringBuffer("spark://")
 				.append(masterIp)
@@ -55,6 +63,8 @@ public class SparkManager {
 		BashSet.execSh(BashSet.start_spkWorker, confOp.toString(), option);
 	}
 	public void stopSparkWorker() {
+		lcdPan = LCDControl.inst.replaceString(lcdPan, "Spark 워커 중지중");
+		lcdPan = LCDControl.inst.removeShapeTimer(lcdPan, 2000);
 		sparkLogger.log(Level.INFO, "스파크 워커 중지");
 		BashSet.execSh(BashSet.stop_spkWorker);
 	}
@@ -75,13 +85,19 @@ public class SparkManager {
 	
 	
 	public boolean initSpark() {
+		lcdPan = LCDControl.inst.showString(lcdX, lcdY, "스파크 초기화 중");
 		sparkLogger.log(Level.INFO, "스파크 초기화 중..");
 		confSpark();
 		String haveSpark = BashSet.execSh(BashSet.check_spark, sparkInstDir);
 		if(haveSpark.equals("false" + CommandExecutor.lineSeparator) ) {
+			lcdPan = LCDControl.inst.replaceString(lcdPan, "스파크 초기화 오류");
+			lcdPan = LCDControl.inst.blinkShape(lcdPan, 500, 6);
+			lcdPan = LCDControl.inst.removeShapeTimer(lcdPan, 3010);
 			sparkLogger.log(Level.SEVERE, "Spark is Missing", new Exception("Spark is Missing"));
 			return false;
 		}
+		lcdPan = LCDControl.inst.replaceString(lcdPan, " ");
+		
 		return true;
 	}
 }
