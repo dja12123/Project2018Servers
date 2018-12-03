7d3bf3716309ae59048ac89145f356694dbc9fa6
dja12123 <dja12123@dja-univ-main>
2018-12-03 17:32:53 +0900

    tet

--- nodeControlServer/src/main/java/node/detection/masterNode/MasterNodeService.java
+++ nodeControlServer/src/main/java/node/detection/masterNode/MasterNodeService.java
@@ -160,7 +160,7 @@ public class MasterNodeService implements Runnable
 					uid = uid.substring(uid.length() - 4, uid.length() - 1);
 					LCDControl.inst.blinkShape(this.stateStr, 2000, 1);
 					byte[] addr = inetAddr.getAddress();
-					LCDControl.inst.removeShapeTimer(LCDControl.inst.showString(40, 0, String.format("접근:%s:%d", uid, 1234)), 1900);
+					LCDControl.inst.removeShapeTimer(LCDControl.inst.showString(40, 0, String.format("접근:%s:%d", uid, addr[3])), 1900);
 				}
 			}
 			if(data.key.equals(KPROTO_MASTER_BROADCAST))
